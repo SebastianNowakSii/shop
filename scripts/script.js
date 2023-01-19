378988b2061efd26ef4e5d45d818660e6c117408
Sebastian Nowak <snowak1@sii.pl>
2023-01-19 16:34:03 +0100

    the main idea drafted

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+console.log('it works');
+
+// const shopItems = document.querySelector(".shop-items");
+
+// shopItems.innerHTML = item;
+
+// let imgSrc = ``;
+// let product = "Drugs";
+// let manufacturer = "Snoop Dog";
+// let description = "japierdolejakiedobre";
+
+// const item = `
+//     <img src="${imgSrc}" alt="${product}"/>
+//     <h4>${product}</h4>
+//     <h5>${manufacturer}</h5>
+//     <p>${description}</p>
+// `;
+// shopItems.innerHTML = item;
+
+// const myFragment = document.createRange().createContextualFragment(item);
+// shopItems.appendChild(myFragment);
