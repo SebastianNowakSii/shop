d8132a2cceda8ebc6128523e4922e72a80f63e81
Sebastian Nowak <snowak1@sii.pl>
2023-01-22 21:20:36 +0100

    displaying works

---
+++ scripts/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: 0,
+        name: "Książka 1",
+        manufacturer: "Wydawnictwo Literackie",
+        price: 29.99,
+        description: "Ostatni raz płakałem tak czytając O psie, który jeżdził koleją",
+    },
+    {
+        id: 1,
+        name: "Książka 2",
+        manufacturer: "Wydawnictwo Literackie",
+        price: .99,
+        description: "Beznadzieja, straszne to jest",
+    },
+    {
+        id: 2,
+        name: "Książka 3",
+        manufacturer: "Wydawnictwo Literackie",
+        price: 29.99,
+        description: "Ostatni raz płakałem tak czytając O psie, który jeżdził koleją",
+    },
+    {
+        id: 3,
+        name: "Książka 4",
+        manufacturer: "Wydawnictwo Literackie",
+        price: .99,
+        description: "Beznadzieja, straszne to jest",
+    },
+];
+
+export default products;
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,40 @@
-console.log('it works');
+import products from './products.js';
 
-// const shopItems = document.querySelector(".shop-items");
 
-// shopItems.innerHTML = item;
+const shopItems = document.querySelector(".shop-items");
 
-// let imgSrc = ``;
-// let product = "Drugs";
-// let manufacturer = "Snoop Dog";
-// let description = "japierdolejakiedobre";
+function displayItems() {
+    products.forEach( (product) => {
+        const item = `
+            <div class="shop-item">
+                <img src="${product.imgSrc}" alt="${product.name}"/>
+                <h4>${product.name}</h4>
+                <h5>${product.manufacturer}</h5>
+                <h5>${product.manufacturer}</h5>
+                <ul class="shoping-details">
+                    <li class="price">${product.price}</li>
+                    <li class="quantity">${product.quantity}</li>
+                    <li class="add-remove"><button>+</button><button>-</button></li>
+                    <li class="add-to-cart"><button><i class="ph-shopping-cart-bold"></i></button></li>
+                </ul>
+            </div>
+        `;
+        shopItems.innerHTML += item;
+    });
+};
 
-// const item = `
-//     <img src="${imgSrc}" alt="${product}"/>
-//     <h4>${product}</h4>
-//     <h5>${manufacturer}</h5>
-//     <p>${description}</p>
-// `;
-// shopItems.innerHTML = item;
+displayItems();
+
+
+
+
+
+
+
+
+
+
+
 
 // const myFragment = document.createRange().createContextualFragment(item);
 // shopItems.appendChild(myFragment);
